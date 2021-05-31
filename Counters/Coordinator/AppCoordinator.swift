d4aa2eca5710256b3a52d7e17828e9cf63736aa3
Fábio Salata <s2it_fsalata@uolinc.com>
2021-05-31 10:21:20 -0300

    configuring project

---
+++ Counters/Coordinator/AppCoordinator.swift
@@ -0,0 +1,20 @@
+//
+//  AppCoordiantor.swift
+//  MoodDetector
+//
+//  Created by Fabio Cezar Salata on 14/04/21.
+//
+
+import UIKit
+
+class AppCoordinator: Coordinator {
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        
+    }
+}
