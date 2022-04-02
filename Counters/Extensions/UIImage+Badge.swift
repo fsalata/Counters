1093fbc1d038725f657253505124f258252ceba2
Fábio Salata <fsalata@gmail.com>
2022-04-02 12:21:09 -0300

    downsample image method

--- Counters/Extensions/UIImage+Badge.swift
+++ Counters/Extensions/UIImage+Badge.swift
@@ -38,4 +38,23 @@ internal extension UIImage {
         }
         return UIGraphicsGetImageFromCurrentImageContext()
     }
+    
+    func downsample(imageAt imageURL: URL, to pointSize: CGSize, scale: CGFloat) -> UIImage? {
+        let imageSourceOptions = [kCGImageSourceShouldCache: false] as CFDictionary
+        guard let imageSource = CGImageSourceCreateWithURL(imageURL as CFURL, imageSourceOptions) else { return nil }
+        
+        let maxDimensionInPixels = max(pointSize.width, pointSize.height) * scale
+        let downsampleOptions = [
+                                    kCGImageSourceCreateThumbnailFromImageAlways: true,
+                                    kCGImageSourceShouldCacheImmediately: true,
+                                    kCGImageSourceCreateThumbnailWithTransform: true,
+                                    kCGImageSourceThumbnailMaxPixelSize: maxDimensionInPixels
+                                ] as CFDictionary
+        
+        if let downsampledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, downsampleOptions) {
+            return UIImage(cgImage: downsampledImage)
+        }
+        
+        return nil
+    }
 }
