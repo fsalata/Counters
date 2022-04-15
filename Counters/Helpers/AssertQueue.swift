38ea1f5d6aadf42fe4984d6aa39176f897a4c776
Fábio Salata <fsalata@gmail.com>
2022-04-15 16:09:54 -0300

    assert queue

---
+++ Counters/Helpers/AssertQueue.swift
@@ -0,0 +1,20 @@
+//
+//  AssertQueue.swift
+//  Counters
+//
+//  Created by Fabio Salata on 15/04/22.
+//
+
+import Foundation
+
+@inline(__always) @inlinable
+public func assertQueue(_ Q: DispatchQueue,
+                        file: StaticString = #file, line: UInt = #line)
+{
+  assert(_dispatchPreconditionTest(.onQueue(Q)), file: file, line: line)
+}
+@inline(__always) @inlinable
+public func assertMainQueue(file: StaticString = #file, line: UInt = #line) {
+  assert(_dispatchPreconditionTest(.onQueue(DispatchQueue.main)),
+         file: file, line: line)
+}
